68bbcbe6f19f25a056890fda0160ba6b1d002500
ralfcrafterytg <106226611+ralfcrafterytg@users.noreply.github.com>
2023-06-18 14:55:21 +0800

    Update scripgg.lua

--- scripgg.lua
+++ scripgg.lua
@@ -3,3 +3,25 @@ local keys = {
   { key ="F84-UmFsZkNyYWZ0ZXI" },
   { key ="F84-RlVDS0lORyBGQVJMSUdIVA" }
 }
+
+keylogi = gg.prompt({ "🇰 🇪 🇾 " }, {}, { "text" })
+
+function checkKey()
+  local enteredKey = keylogi[1]
+  local isKeyValid = false
+
+  for _, k in ipairs(keys) do
+    if k.key == enteredKey then
+      isKeyValid = true
+      break
+    end
+  end
+
+  if isKeyValid then
+    HOME()
+  else
+    os.exit(print("Invalid key. Script execution stopped."))
+  end
+end
+
+checkKey()
