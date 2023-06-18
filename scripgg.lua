ccba4f9b488b495d1e214c28503b4b4f9fec0028
ralfcrafterytg <106226611+ralfcrafterytg@users.noreply.github.com>
2023-06-18 13:02:33 +0800

    Update scripgg.lua

--- scripgg.lua
+++ scripgg.lua
@@ -1,14 +1,12 @@
 KEYPS=[====[
 KEYRS_1 ="F84-UmFsZkNyYWZ0ZXI"
-KEYRS_2 ="1234"
+KEYRS_2 ="F84-RlVDS0lORyBGQVJMSUdIVA"
 ]====]
-aslert= [====[gg.alert("LOL")]====]
+
 
 WRIKEP=[====[
-if pas[1] == KEYRS_1 then 
-PASHING = KEYRS_1
-  end
-if pas[1] == KEYRS_2 then
- PASHING = KEYRS_2 
-  end
+if Result[1] == KEYRS_1
+or
+Result[1] == KEYRS_2
+then 
 ]====]
