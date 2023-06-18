736c647470234335b2b15167839ca78bd78cc3b8
ralfcrafterytg <106226611+ralfcrafterytg@users.noreply.github.com>
2023-06-18 13:08:05 +0800

    Update scripgg.lua

--- scripgg.lua
+++ scripgg.lua
@@ -5,6 +5,9 @@ KEYRS_2 ="F84-RlVDS0lORyBGQVJMSUdIVA"
 
 
 WRIKEP=[====[
+
+Result = gg.prompt({"🇰 🇪 🇾 "},{[1] = "🇰 🇪 🇾"},{"text"})
+if Result[1] == nil or Result[1] == "🇰 🇪 🇾" then else
 if Result[1] == KEYRS_1
 or
 Result[1] == KEYRS_2
@@ -16,5 +19,6 @@ RALF = gg.alert("   ⚠WRONG KEY⚠  ","TRY AGAIN","EXIT")
 if RALF == 1 then goto meme end
 if RALF == 2 then os.exit() end
 return
+end
 end
 ]====]
